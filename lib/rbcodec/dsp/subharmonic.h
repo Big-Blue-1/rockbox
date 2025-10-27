3a428fb2d6b492da8bc82492b5ac654fd5cb9278
Big-Blue-1 <168529170+Big-Blue-1@users.noreply.github.com>
2025-10-27 21:41:20 +0100

    Add subharmonic synthesizer header file

---
+++ lib/rbcodec/dsp/subharmonic.h
@@ -0,0 +1,6 @@
+// Subharmonic Synthesizer for Rockbox, by Vanessa, 2025
+
+void sound_set_subharmonic_enable(bool on);
+void sound_set_subharmonic_crossover(int hz);
+void sound_set_subharmonic_level(int db);
+void sound_set_subharmonic_pregain_enable(bool on);
